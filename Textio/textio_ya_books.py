c0f48ef5caf4286f1d62020e3d6cd7f3bb223011
Emrah Cimren <cimren.1@gmail.com>
2019-03-23 22:20:29 -0700

    setup textio file

---
+++ Textio/textio_ya_books.py
@@ -0,0 +1,17 @@
+'''
+Elif Ilke Gokce's answer to the Textio interview question
+What factors make a Young Adult author more likely to be successful?
+Assume that success means a rating of 4.5 or above and a reviewer count of 100 or above
+'''
+
+def read_input_data():
+
+    
+
+
+def predict_author_success():
+
+
+
+if __name__ == '__main__':
+    main()
