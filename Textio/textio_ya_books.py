0e6eb8d1b6e3ae33114fa4e7354deb91d28f494e
Emrah Cimren <cimren.1@gmail.com>
2019-03-23 22:24:25 -0700

    setup textio file

--- Textio/textio_ya_books.py
+++ Textio/textio_ya_books.py
@@ -4,14 +4,16 @@ What factors make a Young Adult author more likely to be successful?
 Assume that success means a rating of 4.5 or above and a reviewer count of 100 or above
 '''
 
-def read_input_data():
+import pandas as pd
 
-    
+def prepare_data(input_data):
+
+
 
 
 def predict_author_success():
 
-
+    input_data = pd.read_csv('2016 YA books.csv', as_index=False)
 
 if __name__ == '__main__':
     main()
